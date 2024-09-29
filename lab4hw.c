885ff4bd53dd5b52e1585502af126fae437d2133
Ugriumova <vikaezhevika1408@gmail.com>
2024-09-29 18:52:33 +0300

    Add files via upload

---
+++ lab4hw.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#define      N       52
+float ras(float x)
+{
+	float res;
+	res = N * (((4*x)-x)/3);
+}
+void main(void)
+
+{
+	setlocale(LC_ALL, "RUS");
+	float x, res;
+	//ввод данных
+	puts("Введите количество денег, которые трятятся на сладости");
+	scanf_s("%f", &x);
+	//вызов функцию для нахождения значения
+	res = ras(x);
+	//вывод результата
+	printf("Сбережения за год = %2.2f\n",res);
+	system("pause");
+}
